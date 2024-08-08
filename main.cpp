621fc18c73a68745283fe73e8094e5abd801ea4e
SaiSampathKedari <sampath@umich.edu>
2024-08-08 11:14:40 -0400

    Versioning Source Code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "adder.h"
+#include <OLASConfig.h>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello Sampath" << std::endl;
-
     std::cout << add(72.1f, 5.8f) << std::endl;
+
+    std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
     return 0;
 }
