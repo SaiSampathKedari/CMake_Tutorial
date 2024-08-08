d82c963c9340be303f51b38d5554b50e66444216
SaiSampathKedari <sampath@umich.edu>
2024-08-08 11:38:22 -0400

    Making Libs Optional

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include "adder.h"
+// #include "adder.h"
 #include <OLASConfig.h>
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello Sampath" << std::endl;
-    std::cout << add(72.1f, 5.8f) << std::endl;
+
+    #ifdef USE_ADDER
+        std::cout << add(72.1f, 5.8f) << std::endl;
+    #else
+        std::cout << "Adder Lib Not adder" << std::endl;
+    #endif
 
     std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
     return 0;
